dc1068dc6e3104521ab075780ec3de240a4eef28
Charles-M5 <31874901+Charles-M5@users.noreply.github.com>
2017-11-21 18:01:51 -0500

    Initial commit

---
+++ Person.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace LAB02
+{
+    class Person
+    {
+        public int Age;
+        public string firstName;
+        public string lastName;
+
+        public static double sumOfAllAges;
+        public static double averageAge; // The average age is sumOfAllAges / 4
+
+        public void PrintNameAndAge()
+        {
+
+        }
+    }
+}
+    
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace LAB02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person somePerson = new Person();
+
+            Person p1 = new Person();
+            Person spouse = new Person();
+
+            p1.firstName = "Jane";
+            p1.lastName = "doe";
+            p1.Age = 22;
+
+            spouse.firstName = "John";
+            spouse.lastName = "doe";
+            spouse.Age = 34;
+
+            Person p2 = new Person();
+
+            p2.firstName = "David";
+            p2.lastName = "Smith";
+            p2.Age = 76;
+
+            spouse.firstName = "Dora";
+            spouse.lastName = "Smith";
+            spouse.Age = 64;
+        }
+    }
+}
